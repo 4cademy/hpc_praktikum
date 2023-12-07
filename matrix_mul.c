88ee5f8990c5cc8d3625002212c110101e25f693
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-12-07 16:28:06 +0100

    Implemented a matrix multiplication function that uses tiling. Speed up of matrix multiplication for large matrices

--- matrix_mul.c
+++ matrix_mul.c
@@ -13,6 +13,63 @@ void normal_matrix_mul(const float* mat1, const float* mat2, float* mat_out, int
     }
 }
 
+void tiling_matrix_mul(const float* mat1, const float* mat2, float* mat_out, int n, int tiling_factor, int tile_size) {
+    float* result_tile = malloc(tile_size*tile_size*sizeof(float));
+    float* mat1_tile = malloc(tile_size*tile_size*sizeof(float));
+    float* mat2_tile = malloc(tile_size*tile_size*sizeof(float));
+    for (int tx_out = 0; tx_out < tiling_factor; tx_out++) {
+        for (int ty_out = 0; ty_out < tiling_factor; ty_out++) {
+            // copy tile from mat1
+            for (int i = 0; i < tile_size; i++) {
+                for (int j = 0; j < tile_size; j++) {
+                    mat1_tile[i*tile_size+j] = mat1[(((ty_out*tile_size)+i)*n)+((0 * tile_size) + j)];
+                }
+            }
+
+            // copy tile from mat2
+            for (int i = 0; i < tile_size; i++) {
+                for (int j = 0; j < tile_size; j++) {
+                    mat2_tile[i*tile_size+j] = mat2[(((0 * tile_size) + i) * n) + ((tx_out * tile_size) + j)];
+                }
+            }
+            // multiply tiles
+            normal_matrix_mul(mat1_tile, mat2_tile, result_tile, tile_size);
+            // copy result tile to mat_out
+            for (int i = 0; i < tile_size; i++) {
+                for (int j = 0; j < tile_size; j++) {
+                    mat_out[(((ty_out*tile_size)+i)*n)+((tx_out * tile_size) + j)] += result_tile[i*tile_size+j];
+                }
+            }
+            for (int tile_index = 1; tile_index < tiling_factor; tile_index++){
+                // copy tile from mat1
+                for (int i = 0; i < tile_size; i++) {
+                    for (int j = 0; j < tile_size; j++) {
+                        mat1_tile[i*tile_size+j] = mat1[(((ty_out*tile_size)+i)*n)+((tile_index * tile_size) + j)];
+                    }
+                }
+
+                // copy tile from mat2
+                for (int i = 0; i < tile_size; i++) {
+                    for (int j = 0; j < tile_size; j++) {
+                        mat2_tile[i*tile_size+j] = mat2[(((tile_index * tile_size) + i) * n) + ((tx_out * tile_size) + j)];
+                    }
+                }
+                // multiply tiles
+                normal_matrix_mul(mat1_tile, mat2_tile, result_tile, tile_size);
+                // copy result tile to mat_out
+                for (int i = 0; i < tile_size; i++) {
+                    for (int j = 0; j < tile_size; j++) {
+                        mat_out[(((ty_out*tile_size)+i)*n)+((tx_out * tile_size) + j)] += result_tile[i*tile_size+j];
+                    }
+                }
+            }
+        }
+    }
+    free(result_tile);
+    free(mat1_tile);
+    free(mat2_tile);
+}
+
 int main() {
     int n = 256;
     printf("Time in [sec] for %ix%i:\n",n,n);
@@ -33,8 +90,17 @@ int main() {
     }
 
     for (int e = 0; e < evals; e++) {
+        // reset output matrix
+        for (int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                mat_out[i*n+j] = 0;
+            }
+        }
         // actual matrix multiplication
+        int tiling_factor = 16;
+        int tile_size = n/tiling_factor;
         gettimeofday(&start, NULL);
+        // tiling_matrix_mul(mat1, mat2, mat_out, n, tiling_factor, tile_size);
         normal_matrix_mul(mat1, mat2, mat_out, n);
         gettimeofday(&end, NULL);
         long long microseconds = ((end.tv_sec - start.tv_sec) * 1000*1000 + end.tv_usec - start.tv_usec);
