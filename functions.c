41a02c417d267b5549626fc9c3c63441e8967fb7
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-12-11 14:24:22 +0100

    Fixed loop_swap

--- functions.c
+++ functions.c
@@ -4,6 +4,7 @@
 
 #include "functions.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 void normal_matrix_mul(const float* mat1, const float* mat2, float* mat_out, int n) {
     for (int i = 0; i < n; i++) {
@@ -65,12 +66,12 @@ void loop_unrolling(const float* mat1, const float* mat2, float* mat_out, int n)
     }
 }
 
-void loop_swap(const float* mat1, const float* mat2_inv, float* mat_out, int n) {
+void loop_swap(const float* mat1, const float* mat2, float* mat_out, int n) {
     for (int j = 0; j < n; j++) {
         for(int i = 0; i < n; i++) {
-            mat_out[i*n+j] = mat1[i*n] * mat2_inv[i*n];
+            mat_out[i*n+j] = mat1[i*n] * mat2[j];
             for (int k = 1; k < n; k++) {
-                mat_out[i*n+j] += mat1[i*n+k] * mat2_inv[i*n+k];
+                mat_out[i*n+j] += mat1[i*n+k] * mat2[k*n+j];
             }
         }
     }
