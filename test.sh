a6d0fbf34f365ad8dbed8a3e0a4b138780bf4a73
Martin Carnein <marti.carnein@hotmail.de>
2023-12-11 12:33:22 +0100

    shell script for execution of different loops added

--- test.sh
+++ test.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-./matrix_mul > matmul.txt 2>&1
+#use the according number to run the function in main
+#define NORMAL 0
+#define LOOP_UNROLLING 1
+#define LOOP_SWAP 2
+#define TILING 3
 
+
+
+for i in 0 1 2 3
+do
+    echo "Running $i"
+    ./matrix_mul $i >> matmul.txt 2>&1
+done
