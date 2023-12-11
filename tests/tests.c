3a220ec6a5b11de346a557ea6c24598ee3521089
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-12-11 13:12:36 +0100

    Wrote test for loop_swap and loop_unrolling

--- tests/tests.c
+++ tests/tests.c
@@ -2,6 +2,42 @@
 #include <stdio.h>
 #include "../functions.h"
 
+void print_differences(float* mat1, float* mat2, int n) {
+    printf("mat2_golden:\n");
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            printf("%d\t", (int)(mat1[i*n+j]));
+        }
+        printf("\n");
+    }
+    printf("mat_out:\n");
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if (mat1[i*n+j] != mat2[i*n+j]) {
+                printf("%d\t", (int)(mat2[i*n+j]));
+            } else {
+                printf("EQL\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int check_equal(float* mat1, float* mat2, int n) {
+    int is_equal = 1;
+    for (int i = 0; i < n*n; i++) {
+        if (mat1[i] != mat2[i]) {
+            is_equal = 0;
+            break;
+        }
+    }
+
+    if (!is_equal) {
+        print_differences(mat1, mat2, n);
+    }
+    return is_equal;
+}
+
 int test_normal() {
     int n = 4;
     float* mat1 = (float*) malloc(n*n*sizeof(float));
@@ -36,13 +72,7 @@ int test_normal() {
 
     normal_matrix_mul(mat1, mat2, mat_out, n);
 
-    int is_equal = 1;
-    for (int i = 0; i < n*n; i++) {
-        if (mat2_golden[i] != mat_out[i]) {
-            is_equal = 0;
-            break;
-        }
-    }
+    int is_equal = check_equal(mat2_golden, mat_out, n);
 
     free(mat1);
     free(mat2);
@@ -72,14 +102,7 @@ int test_tiling() {
     normal_matrix_mul(mat1, mat2, mat2_golden, n);
     tiling_matrix_mul(mat1, mat2, mat_out, n, tiling_factor, tile_size);
 
-    int is_equal = 1;
-    for (int i = 0; i < n*n; i++) {
-        if (mat2_golden[i] != mat_out[i]) {
-            printf("i: %i, mat2_golden[i]: %f, mat_out[i]: %f\n", i, mat2_golden[i], mat_out[i]);
-            is_equal = 0;
-            break;
-        }
-    }
+    int is_equal = check_equal(mat2_golden, mat_out, n);
 
     free(mat1);
     free(mat2);
@@ -89,17 +112,86 @@ int test_tiling() {
     return is_equal;
 }
 
+int test_loop_unrolling(){
+    int n = 8;
+    float* mat1 = (float*) malloc(n*n*sizeof(float));
+    float* mat2 = (float*) malloc(n*n*sizeof(float));
+    float* mat_out = malloc(n*n*sizeof(float));
+    float* mat2_golden = (float*) malloc(n*n*sizeof(float));
+
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            mat1[i*n+j] = (float)(i+j);
+            mat2[i*n+j] = (float)(i+j);
+            mat_out[i*n+j] = 0;
+        }
+    }
+
+    normal_matrix_mul(mat1, mat2, mat2_golden, n);
+    loop_unrolling(mat1, mat2, mat_out, n);
+
+    int is_equal = check_equal(mat2_golden, mat_out, n);
+
+    free(mat1);
+    free(mat2);
+    free(mat_out);
+    free(mat2_golden);
+
+    return is_equal;
+}
+
+int test_loop_swap(){
+    int n = 8;
+    float* mat1 = (float*) malloc(n*n*sizeof(float));
+    float* mat2 = (float*) malloc(n*n*sizeof(float));
+    float* mat2_inv = (float*) malloc(n*n*sizeof(float));
+    float* mat_out = malloc(n*n*sizeof(float));
+    float* mat2_golden = (float*) malloc(n*n*sizeof(float));
+
+    for (int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            mat1[i*n+j] = (float)(i+j);
+            mat2[i*n+j] = (float)(i+j);
+            mat2_inv[j*n+i] = (float)(i+j);
+            mat_out[i*n+j] = 0;
+        }
+    }
+
+    normal_matrix_mul(mat1, mat2, mat2_golden, n);
+    loop_swap(mat1, mat2_inv, mat_out, n);
+
+    int is_equal = check_equal(mat2_golden, mat_out, n);
+
+
+    free(mat1);
+    free(mat2);
+    free(mat2_inv);
+    free(mat_out);
+    free(mat2_golden);
+
+    return is_equal;
+}
 
 int main() {
     if (test_normal()) {
-        printf("Normal matrix multiplication: PASSED\n");
+        printf("Normal: PASSED\n");
         if (test_tiling()) {
-            printf("Tiling matrix multiplication: PASSED\n");
+            printf("Tiling: PASSED\n");
         } else {
-            printf("Tiling matrix multiplication: FAILED\n");
+            printf("Tiling: FAILED\n");
+        }
+        if (test_loop_unrolling()) {
+            printf("Loop unrolling: PASSED\n");
+        } else {
+            printf("Loop unrolling: FAILED\n");
+        }
+        if (test_loop_swap()) {
+            printf("Loop swap: PASSED\n");
+        } else {
+            printf("Loop swap: FAILED\n");
         }
     } else {
-        printf("Normal matrix multiplication: FAILED\n");
+        printf("Normal: FAILED\n");
         printf("No further tests will be executed\n");
     }
     return 0;
