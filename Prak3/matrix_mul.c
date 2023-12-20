3eef367ae6321715971f4aa9d7fac24291e8eb1f
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-12-20 15:20:38 +0100

    Changed evals to 1

--- Prak3/matrix_mul.c
+++ Prak3/matrix_mul.c
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
     matrix_size= atoi(argv[1]);
 
     int n = matrix_size;
-    int evals = 10;
+    int evals = 1;
     struct timeval start, end;
     long double measures[10] = {0};
     long double avg_time = 0;
