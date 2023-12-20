4ebec2433b0455703af732b9bb25d736f64881da
mabe447c <marcel.beyer2@mailbox.tu-dresden.de>
2023-12-20 14:45:18 +0100

    Added OpenMP pragmas

--- Prak3/functions.c
+++ Prak3/functions.c
@@ -5,6 +5,7 @@
 #include "functions.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <omp.h>
 
 void normal_matrix_mul(const float* mat1, const float* mat2, float* mat_out, int n) {
     for (int i = 0; i < n; i++) {
@@ -18,6 +19,8 @@ void normal_matrix_mul(const float* mat1, const float* mat2, float* mat_out, int
 }
 
 void openmp_matrix_mul(const float* mat1, const float* mat2, float* mat_out, int n) {
+#pragma omp target map(to:n,mat1[0:n],mat2[0:n]) map(from:mat_out[0:n])
+#pragma omp teams distribute parallel for
     for (int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
             mat_out[i*n+j] = mat1[i*n] * mat2[j];
