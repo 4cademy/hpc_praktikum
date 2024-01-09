80b991effb8fde3e138ecc3fb9c7d9a0795e8121
martin <marti.carnein@hotmail.de>
2024-01-09 15:14:53 +0100

    still persisting error

--- Prak3/modules.sh
+++ Prak3/modules.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+
 module purge
 echo "module purge"
 sleep 2
 module switch modenv/ml
 echo "module switch modenv/ml"
 sleep 2
-#module load NVHPC
-#echo "module load NVHPC"
-#sleep 2
 module load GCC/8.3.0
 echo "module load GCC/8.3.0"
+sleep 2
+module load NVHPC
+echo "module load NVHPC"
 sleep 2
